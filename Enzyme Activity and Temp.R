8929162e9231d1801f44f71ea9d4b89b9ef31be3
Souplarz <ssularz@uvm.edu>
2024-09-30 13:44:02 -0400

    emzyme lab analysis

--- Enzyme Activity and Temp.R
+++ Enzyme Activity and Temp.R
@@ -13,11 +13,23 @@ head(enzymedata)
 
 
 #2-way ANOVA
-x2anova <- aov(IU.gfw ~ species * temp, data = enzymedata)
+x2anova <- aov(IU.gfw ~ temp * species, data = enzymedata)
 summary(x2anova)
 
 #graph something 
-
+ggplot(enzymedata, aes(x = temp, y = IU.gfw, color = species)) +
+  geom_point() +                  # Plot data points
+  #geom_smooth(method = "lm", se = FALSE) +  # Add linear regression lines #they suck so im taking them out
+  labs(title = "MDH Activity (IU/gfw) vs Temperature",
+       x = "Temperature (°C)",
+       y = "MDH Activity (IU/gfw)") +
+  theme_minimal() +                       # Use a clean theme
+  scale_color_manual(values = c("blue", "red")) + # Different colors for each species
+  theme(legend.title = element_blank()) +    # Remove legend title
+  xlim(15, 40) +
+  ylim(20, 180)
 
 
 #Q10 
+#Q10 = (R 2 /R 1 )^(10/(T2 - T1 )
+
