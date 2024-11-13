9ef91b3d2786cd396998581ed1646fc471b552a9
Souplarz <ssularz@uvm.edu>
2024-11-13 16:44:16 -0500

    Plan analysis of proj data, create working script, create folder for docs and figures

---
+++ Independent Proj/docs_figures/Developmental_Nicotine.R
@@ -0,0 +1,47 @@
+##Sage Sularz 
+##Comparitive Physiology 
+##Fall 2024
+
+
+##          A Drosophila Model for Developmental Nicotine Exposure                     # #
+##########################################################################################
+#
+# If Drosophila melanogaster consume a diet containing nicotine throughout their lives, they will
+#show a preference for food with nicotine over a control and a marked decrease in fittness
+#compared to fruit flies that have never ingested nicotine. Flies introduced to nicotine 
+#during adulthood will show moderate effect when compared to control and developmental
+#
+##########################################################################################
+
+
+############################################################################
+##
+# 1. Effect of nicotine concentration and life stage on climbing speed
+##
+############################################################################
+
+
+
+############################################################################
+##
+# 2. Effect of nicotine concentration in development on puparium length
+##
+############################################################################
+
+
+
+
+############################################################################
+##
+# 3. Effect of nicotine concentration and life stage on food preference
+##
+############################################################################
+
+
+
+
+
+
+
+
+
