606636120a1a29c658fe746ac606c0bad7df1cdc
Souplarz <ssularz@uvm.edu>
2024-11-18 15:15:00 -0500

    Prepare code for data anaylsis

--- Independent Proj/docs_figures/Developmental_Nicotine.R
+++ Independent Proj/docs_figures/Developmental_Nicotine.R
@@ -12,7 +12,15 @@
 #during adulthood will show moderate effect when compared to control and developmental
 #
 ##########################################################################################
+#Library checkout
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 
+setwd("~/Desktop/School /Comp. Physiology/LabWork/comp_physiology/Independent Proj/")
+#call data
+nicdata <- read.csv(file = "nic_data.csv") 
+head(nicdata)
 
 ############################################################################
 ##
@@ -20,6 +28,20 @@
 ##
 ############################################################################
 
+#ANOVA
+climbinganova <- aov(speed ~ nic_dose * treatment, data = nicdata)
+summary(climbinganova)
+
+#sink("climbinganova.txt")  
+#print(climbinganova)   
+#sink() 
+
+ggplot(nicdata, aes(x=nic_dose, y=speed, fill=treatment)) +
+  geom_boxplot() +
+  labs(x="Nicotine Doseage", y="Climbing Speed (mm/sec)") +
+  scale_fill_manual(values=c("adult"="lightblue3", "dev"="tomato")) +
+  theme_minimal() +
+  theme(legend.position = "top") 
 
 
 ############################################################################
@@ -28,8 +50,47 @@
 ##
 ############################################################################
 
+nicdata_filtered <- nicdata %>% filter(!is.na(length))
+head(nicdata_filtered)
+
+pupaeanova <- aov(length ~ nic_dose, data = nicdata_filtered)
+summary(pupaeanova)
 
 
+#sink("pupaeTtest.txt") 
+#print(pupaeTtest)       
+#sink()                     
+
+ggplot(nicdata_filtered, aes(x=nic_dose, y=length)) +
+  geom_boxplot() +
+  labs(x="Nicotine Doseage", y="Pupae Length (mm)") +
+  theme_minimal() +
+  theme(legend.position = "top") 
+
+p1 <- ggplot(nicdata_filtered, aes(x = nic_dose, y = length)) +
+  geom_violin(trim = FALSE, color = "black", fill = "lightblue") +
+  geom_boxplot(width = 0.1, position = position_dodge(0.9), outlier.shape = NA) +
+  stat_summary(
+    fun.data = function(x) {
+      data.frame(
+        ymin = quantile(x, 0.05),
+        ymax = quantile(x, 0.95),
+        y = median(x)
+      )
+    },
+    geom = "errorbar", 
+    width = 0.2, 
+    color = "black"
+  ) +
+  labs(
+    title = "Grouped Box & Violin Plot for Nicotine Dose",
+    x = "Nicotine Dose",
+    y = "Length"
+  ) +
+  theme_minimal()
+  
+
+p1
 
 ############################################################################
 ##
